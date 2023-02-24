545087cb535bc3a6de4ee79c3844330ad2dd1de9
armin-faldis <armin.fallah96@gmail.com>
2023-02-24 12:53:10 +0330

    Implement quick sort

---
+++ quick-sort/src/main.rs
@@ -0,0 +1,33 @@
+use rand::Rng;
+
+fn main() {
+    let mut array = vec![5, 1, -10, 3, 100, 20000, 77, 6, 7, -1, 0];
+
+    quick_sort(&mut array, 0, 11);
+
+    println!("{:?}", array);
+}
+
+fn quick_sort<T: Ord>(array: &mut Vec<T>, start: usize, end: usize) {
+    if end - start <= 1 {
+        return;
+    }
+
+    let pivot = rand::thread_rng().gen_range(start..end);
+    array.swap(start, pivot);
+
+    let mut seperator_index = start;
+
+    for i in (start + 1)..end {
+        if array.get(i) < array.get(start) {
+            array.swap(i, seperator_index + 1);
+
+            seperator_index += 1;
+        }
+    }
+
+    array.swap(start, seperator_index);
+
+    quick_sort(array, start, seperator_index + 1);
+    quick_sort(array, seperator_index + 1, end);
+}
